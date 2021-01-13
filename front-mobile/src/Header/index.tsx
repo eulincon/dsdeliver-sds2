49dadf51f7c408c700912fcb101bb560a18f94c6
Lincon <linconsilva08@gmail.com>
2021-01-13 20:19:12 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,20 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Header() {
+	const navigation = useNavigation()
+	const handleOnPress = () => {
+		navigation.navigate('Home')
+	}
 	return (
-		<View style={styles.container}>
-			<Image source={require('../assets/logo.png')} />
-			<Text style={styles.text}>DS Delivery</Text>
-		</View>
+		<TouchableWithoutFeedback onPress={handleOnPress}>
+			<View style={styles.container}>
+				<Image source={require('../assets/logo.png')} />
+				<Text style={styles.text}>DS Delivery</Text>
+			</View>
+		</TouchableWithoutFeedback>
 	)
 }
 
